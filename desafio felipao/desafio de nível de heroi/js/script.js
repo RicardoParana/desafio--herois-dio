532c704667735f62f9581de558688d45930edd9e
Ricardo <105986807+RicardoParana@users.noreply.github.com>
2023-10-29 21:00:09 -0300

    Add files via upload

---
+++ desafio felipao/desafio de nível de heroi/js/script.js
@@ -0,0 +1,20 @@
+let nickname = "bilo-bilo"
+let quantidadeXp = 10001;
+
+if(quantidadeXp <= 1000){
+  console.log("O herói "  + nickname + " está no nível ferro");
+}else if(quantidadeXp <= 2000){
+  console.log("O herói "  + nickname + " está no nível bronze");
+}else if(quantidadeXp <= 6000){
+  console.log("O herói "  + nickname + " está no nível prata");
+}else if(quantidadeXp <= 7000){
+  console.log("O herói "  + nickname + " está no nível ouro");
+}else if(quantidadeXp <= 8000){
+  console.log("O herói "  + nickname + " está no nível platina");
+}else if(quantidadeXp <= 9000){
+  console.log("O herói "  + nickname + " está no nível ascendente");
+}else if(quantidadeXp <= 10000){
+  console.log("O herói "  + nickname + " está no nível imortal");
+}else{
+  console.log("O herói "  + nickname + " está no nível radiante");
+}
